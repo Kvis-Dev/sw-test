d3261babea555b0ad5f038487d7b44c875d56037
Kvis-Dev <kvis.dev@gmail.com>
2016-08-24 11:59:07 +0300

    +

---
+++ db/config.py
@@ -0,0 +1,4 @@
+DBNAME = 'swt'
+DBADDR = '127.0.0.1'  #127.0.0.1
+DBUSER = 'root'
+DBPASS = 'root'
--- swtest/settings.py
+++ swtest/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -74,12 +75,12 @@ WSGI_APPLICATION = 'swtest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
